815429b35d335f3a28241e58a214192c5d1fa191
Soroush Mostofi Rad <soroush.mrad@gmail.com>
2020-10-23 10:39:42 +0330

    Update Comment for Test

--- IT/urls.py
+++ IT/urls.py
@@ -1,4 +1,4 @@
-"""IT URL Configuration
+"""IT URL Configuration!
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
